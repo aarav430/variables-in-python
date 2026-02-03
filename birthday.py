9954c3995ed30952ea8bba9a776ce750a5a63328
aarav430 <aaravpatel276@gmail.com>
2026-02-03 18:13:07 +1000

    first commit

---
+++ birthday.py
@@ -0,0 +1,14 @@
+# to remember peoples birthdays
+
+day="31 12 2010"
+daytwo="15 6 2010"
+daythree="11 8 2010"
+dayfour="1 1 2009"
+dayfive="11 1 1984"
+
+
+print (day)
+print (daytwo)
+print (daythree)
+print (dayfour)
+print (dayfive)
