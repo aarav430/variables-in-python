0655bf04a3d00c2a841c5c6a0be0ed2dceeb35ec
aarav430 <aaravpatel276@gmail.com>
2026-08-08 17:27:32 +1000

    sets and arrays

---
+++ basket.py
@@ -0,0 +1,12 @@
+basket1 = {"apple", "bananna", "grape", "orange"}
+basket2 = {"apple", "orange", "bananna", "dragon fruit", "kiwi"}
+common = basket1.intersection(basket2)
+print(basket1)
+print(basket2)
+print(common)
+fruitcounts = [1 , 3 ,4 ,2]
+fruitcounts.append(4)
+print(fruitcounts)
+print(len(fruitcounts))
+fruitcounts.reverse()
+print(fruitcounts)
