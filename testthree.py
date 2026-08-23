bc943eea024d02545866c7f60f266a977471041b
aarav430 <aaravpatel276@gmail.com>
2026-08-23 17:43:34 +1000

    test 3

---
+++ testthree.py
@@ -0,0 +1,7 @@
+students = {"james": 80, 'oliver': 60, 'samule': 40, 'josh': 90  }
+print(students)
+average = (80 + 60 + 40 + 90) /4
+print(average)
+print(max(students))
+print(min(students))
+name = input("enter student nam here".__getitem__)
